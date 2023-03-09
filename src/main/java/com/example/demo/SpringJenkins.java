f01243bfa62d5adee84f3e9774af0e6df85beccb
krishs09 <krishs09@yahoo.in>
2023-03-09 12:14:10 +0530

    logging added

--- src/main/java/com/example/demo/SpringJenkins.java
+++ src/main/java/com/example/demo/SpringJenkins.java
@@ -7,17 +7,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-public class SpringJenkins extends SpringBootServletInitializer {
-
+@ComponentScan(basePackages= {"com.example.conroller","com.example.demo"})
+//public class SpringJenkins extends SpringBootServletInitializer {
+public class SpringJenkins  {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkins.class);
 	
-	@PostConstruct
-	public void init() {
-		logger.info("Application started..");
+	
+	  @PostConstruct public void init() 
+	  { 
+		  logger.info("Application started.."); 
 	}
+	 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkins.class, args);
